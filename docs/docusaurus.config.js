ff42836b0853d17f064422906ba017fd7942f334
Pablo Zaved <pablo.azevedo@sou.inteli.edu.br>
2025-09-08 23:34:13 -0300

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,10 +8,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://inteliprojects.github.io', // Atualize conforme sua organização
-  baseUrl: '/2025_TRAINEE_EDUCACIONAL_01/',
+  baseUrl: '/2025_TRAINEE_EDUCACIONAL_02/',
 
   organizationName: 'InteliProjects', // Atualize conforme sua organização
-  projectName: '2025_TRAINEE_EDUCACIONAL_01',
+  projectName: '2025_TRAINEE_EDUCACIONAL_02',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -58,7 +58,7 @@ const config = {
             label: 'Documentação',
           },
           {
-            href: 'https://github.com/agrotechinteli/2025_TRAINEE_EDUCACIONAL_01', // Atualize para o URL correto do seu repositório
+            href: 'https://github.com/agrotechinteli/2025_TRAINEE_EDUCACIONAL_02', // Atualize para o URL correto do seu repositório
             label: 'GitHub',
             position: 'right',
           },
